19b84f1bbd2c1aafd3951e9fb9ad958445d1d2d1
Aibek <roninfr1508@gmail.com>
2026-01-17 22:14:59 +0500

    Assigment_Oop

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,9 +8,9 @@ public class Main {
         passengers.add(new Passenger("Aibek", 17));
         passengers.add(new Passenger("Igor", 30));
         passengers.add(new Passenger("Daryn", 19));
-        passengers.add(new Passenger("Tyler", 28));
+        passengers.add(new Passenger("Tayler", 28));
 
-        Passenger p3 = new Passenger("Tyler",28);
+        Passenger p3 = new Passenger("Tayler",28);
         System.out.println(p3);
 
         ArrayList<Person> peaple = new ArrayList<>();
@@ -50,6 +52,57 @@ public class Main {
         for (Identifiable p : people) {
             System.out.println(p.getRole());
         }
+        try (Connection conn = DBConnection.getConnection()) {
+            System.out.println("Connected to database!");
+
+            // Insert flight
+            String insertFlight = "INSERT INTO flights (flight_code, price) VALUES (?, ?)";
+            PreparedStatement psFlight = conn.prepareStatement(insertFlight);
+            psFlight.setString(1, "KC101");
+            psFlight.setInt(2, 15000);
+            psFlight.executeUpdate();
+
+            // Insert passenger
+            String insertPassenger = "INSERT INTO passengers (name, age) VALUES (?, ?)";
+            PreparedStatement psPassenger = conn.prepareStatement(insertPassenger);
+            psPassenger.setString(1, "Aibek");
+            psPassenger.setInt(2, 17);
+            psPassenger.executeUpdate();
+
+            // Read flights
+            ResultSet rsFlights = conn.createStatement().executeQuery("SELECT * FROM flights");
+            while (rsFlights.next()) {
+                System.out.println(rsFlights.getInt("flight_id") + " "
+                        + rsFlights.getString("flight_code") + " "
+                        + rsFlights.getInt("price"));
+            }
+
+            // Read passengers
+            ResultSet rsPassengers = conn.createStatement().executeQuery("SELECT * FROM passengers");
+            while (rsPassengers.next()) {
+                System.out.println(rsPassengers.getInt("passenger_id") + " "
+                        + rsPassengers.getString("name") + " "
+                        + rsPassengers.getInt("age"));
+            }
+
+            // Update
+            String updateFlight = "UPDATE flights SET price = ? WHERE flight_id = ?";
+            PreparedStatement psUpdate = conn.prepareStatement(updateFlight);
+            psUpdate.setInt(1, 17000);
+            psUpdate.setInt(2, 1); // обновляем первый рейс
+            psUpdate.executeUpdate();
+
+            //Delete
+            String deletePassenger = "DELETE FROM passengers WHERE passenger_id = ?";
+            PreparedStatement psDelete = conn.prepareStatement(deletePassenger);
+            psDelete.setInt(1, 1); // удаляем первого пассажира
+            psDelete.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
 
     }
 
